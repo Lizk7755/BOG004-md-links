e07ed97c13515f20309d8b40d74da882d5cf01e8
Francy Corredor <fran_ymile@hotmail.com>
2022-04-28 11:25:00 -0500

    corrección de archivo

--- index.js
+++ index.js
@@ -1,57 +1,25 @@
 'use strict'
+
 const fs = require('fs');
 const https = require('https');
 const path = require('path');
-const { CLIENT_RENEG_LIMIT } = require('tls');
-const route = process.argv[2];
+const process = require(('process'));
 
-const { validateUrl, getObject } = require('functions.js')
-​
 module.exports = (fileMdUrl = '', options = {validate: false}) => {
   let contentMdFile
   let respuesta = {
     data: [],
     errors: ''
   }
-​
+
   const {validate} = options; // destructuring
-​
+
   return new Promise((resolve, reject) => {    
     try {
       //Validar reuta absoluta o relativa
       //Recursividad en caso de que la ruta apunte a un directorio
-//       const userPath = path.resolve(route);
-//       function identifyDir (currentPath) { 
-//         if(userPath.extname === undefined){
-//             let pathArr = [];
-//             let actualPath = '';
-//                 fs.readdirSync(userPath, (err, items) => {
-//                     if(err){
-//                         console.log(err);
-//                     }else{
-//                         items.forEach(item => {
-//                           identifyDir(item)
-//                             if(item.isDirectory()){//validar en la documentación si es directorio
-
-//                             }
-//                             const ext = path.extname(item);
-//                             if(ext === '.md'){
-//                                 actualPath = userPath + '\\' + item;
-//                                 pathArr.push(actualPath);
-//                             }
-//                         })
-//                     }
-//                 })
-//             return pathArr;
-// ​
-//             //RECORRER EL DIRECTORIO => VERIFICAR SI ES UN ARCHIVO => VERIFICAR SI ES MD => ALMACENAR EN ARRAY() //  SI ES UNA CARPERTA => RECORRER DIRECTORIO => VER. ARCHIVO? => RECURSIVIDAD
-//         }else{
-//             archivo
-//             //SI ES ARCHIVO => EVALUAR SI ES MD => GUARDAR EN UN ARRAY
-//         }
+    
       
-//     }
-      // FOREACH 
       contentMdFile = fs.readFileSync(fileMdUrl, {encoding: 'utf-8', flag: 'r'}).toString()
       //Extraer los links
       //Crear el objeto respuesta
@@ -62,7 +30,7 @@ module.exports = (fileMdUrl = '', options = {validate: false}) => {
     
     respuesta.data = getObject(contentMdFile, fileMdUrl)
     //console.log(validate)
-​
+
     if(validate){
       let urlValidatedList = respuesta.data.map(objeto => validateUrl(objeto.href)
         .then( res => {
@@ -85,13 +53,50 @@ module.exports = (fileMdUrl = '', options = {validate: false}) => {
         reject(respuesta.errors)
       }
     }
-​
+
   })
 }; // fin 
-​
-​
+
+function validateUrl(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, res =>  resolve(res))
+      .on('error', e => reject(false));
+  });
+}
+
+function getObject(contentMdFile = '', fileMdUrl = '') {
+  const getLinksRegex = /!*\[(.+?)\]\((.+?)\)/gi
+  let getUrls = contentMdFile.match(getLinksRegex)
+  const respuesta = createObjectResponse(getUrls, fileMdUrl)
+  return respuesta
+}
+
+function createObjectResponse (urls, fileMdUrl) {
+  const createBasicObject = urls.map((url) => {  
+    const splitUrl = url.split("](")
+    const text = splitUrl[0].slice(1)
+    const href = splitUrl[1].slice(0, -1)
+    return {
+      href,
+      text,
+      file: fileMdUrl
+    }
+  })
+  return createBasicObject
+}
+
+// Ejemplo cómo se va a consumir la libreria
+
 const mdLinks = require ('./index.js')
 
-mdLinks("./DirectorioPrueba/ejemploPrueba.md", {validate: true})
+mdLinks("./README.md", {validate: true})
 .then(links => console.log('links: ', links))
 .catch(console.error)
+/* 
+mdLinks("./README.md")
+  .then(links => console.log(links))
+  .catch(console.error);
+mdLinks("./README.md", {validate: false})
+  .then(links => console.log(links))
+  .catch(console.error);
+*/
