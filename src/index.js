a9fb3cc12adb29508e9ca91c6f032f142143dfcf
Lizeth Rodriguez <lizeth_775@hotmail.com>
2022-05-05 15:25:42 -0500

    funcion md-links

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const { rejects } = require("assert");
+const fs = require("fs");
+const { resolve } = require("path");
+const path = require("path");
+const fsPromises = require('fs').promises;
+const clc = require("cli-color");
+
+
